8dc123543cce24b632e31bdd7458f213fb20e164
Sukhpreet Kaur <kau11963@sheridancollege.ca>
2023-01-27 12:56:58 -0500

    parttime new branch

---
+++ src/testfri1/partTimeStudent.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testfri1;
+
+/**
+ *
+ * @author 91628
+ */
+public class partTimeStudent  {
+    private int numofCourse = 6;
+   
+}
